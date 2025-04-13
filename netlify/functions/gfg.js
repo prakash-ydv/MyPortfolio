59a47ebaacbfaba507cb2cb0683b51c488fb4e06
Prakash Kumar <prakashyadav9576@gmail.com>
2025-04-13 20:12:48 +0530

    api fix

--- netlify/functions/gfg.js
+++ netlify/functions/gfg.js
@@ -1,28 +1,27 @@
-// netlify/functions/gfg.js
 import fetch from 'node-fetch';
 
 export const handler = async (event) => {
-  // Extract the username from the URL path
   const username = event.path.split("/").pop();
-  
+
   try {
-    // Fetch data from GeeksforGeeks API
     const response = await fetch(`https://geeks-for-geeks-api.vercel.app/${username}`);
     const data = await response.json();
-    
-    // Return the fetched data as JSON
+
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*", // Allow cross-origin requests
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     };
   } catch (error) {
-    // Handle errors if any
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Unable to fetch data from GeeksforGeeks API.' }),
+      body: JSON.stringify({
+        message: 'Failed to fetch from GFG API',
+        error: error.message,
+      }),
     };
   }
 };
